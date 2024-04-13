13f77c8f844d4715ae5ea98cd423fe47d0797c8c
Wuttyi <ms.hninwuttyi@gmail.com>
2024-04-13 22:29:53 +0700

    Connect with database and c#.

--- HWYDotNetCore.ConsoleApp/Program.cs
+++ HWYDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+//Ctrl + .  => Suggestion
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "HNINWUTTYI\\MSSQLSERVER2012"; //server name
+stringBuilder.InitialCatalog = "HWYDotNetCore"; //database name
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "admin123!";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection Open");
+
+connection.Close();
+Console.WriteLine("Connection Close");
+
+Console.ReadKey();
