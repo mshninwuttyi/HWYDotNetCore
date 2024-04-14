23ddacdf46e61c1c9f425315a16352aefa87bca2
Wuttyi <ms.hninwuttyi@gmail.com>
2024-04-14 12:17:23 +0700

    [Completed] Ado.Net CRUD

--- HWYDotNetCore.ConsoleApp/Program.cs
+++ HWYDotNetCore.ConsoleApp/Program.cs
@@ -1,20 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HWYDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
 //Ctrl + .  => Suggestion
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "HNINWUTTYI\\MSSQLSERVER2012"; //server name
-stringBuilder.InitialCatalog = "HWYDotNetCore"; //database name
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "admin123!";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("Connection Open");
-
-connection.Close();
-Console.WriteLine("Connection Close");
-
-Console.ReadKey();
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(13, "testing title", "test author", "test content");
+//adoDotNetExample.Delete(13);
+adoDotNetExample.Edit(13);
+adoDotNetExample.Edit(1);
+Console.ReadLine();
