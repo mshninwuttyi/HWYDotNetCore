d30ecd49459264766de0803438cb356bc9b72ec5
Wuttyi <ms.hninwuttyi@gmail.com>
2024-05-25 15:47:16 +0700

    Complete  Myanmar Months API

---
+++ HWYDotNetCore.RestApiWithNLayer/Features/MyanmarMonths/MyanmarMonthsController.cs
@@ -0,0 +1,50 @@
+﻿using HWYDotNetCore.RestApiWithNLayer.Features.MyanmarProverbs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HWYDotNetCore.RestApiWithNLayer.Features.MyanmarMonths
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarMonthsController : ControllerBase
+    {
+        private async Task<Rootobject> GetDataFromJsonData()
+        {
+            var jsonStr = await System.IO.File.ReadAllTextAsync("data3.json");
+            var model = JsonConvert.DeserializeObject<Rootobject>(jsonStr);
+            return model;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var model = await GetDataFromJsonData();
+            return Ok(model.Tbl_Months);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var model = await GetDataFromJsonData();
+            var item = model.Tbl_Months.FirstOrDefault(x => x.Id == id);
+            return Ok(item);
+        }
+    }
+
+    public class Rootobject
+    {
+        public Tbl_Months[] Tbl_Months { get; set; }
+    }
+
+    public class Tbl_Months
+    {
+        public int Id { get; set; }
+        public string MonthMm { get; set; }
+        public string MonthEn { get; set; }
+        public string FestivalMm { get; set; }
+        public string FestivalEn { get; set; }
+        public string Description { get; set; }
+        public string Detail { get; set; }
+    }
+}
