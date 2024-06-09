797c6001e062f00b49d224da103f175726db8802
Wuttyi <ms.hninwuttyi@gmail.com>
2024-06-09 13:54:38 +0700

    Complete NLayer

---
+++ HWYDotNetCore.NLayer.BusinessLogic/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HWYDotNetCore.NLayer.BusinessLogic
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ HWYDotNetCore.NLayer.DataAccess/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using HWYDotNetCore.NLayer.DataAccess.Models;
+
+namespace HWYDotNetCore.NLayer.DataAccess.Database
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
