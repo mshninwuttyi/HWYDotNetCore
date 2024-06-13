b3d6859ad3e1e91fdb455cc12ab31800a0e7d159
Wuttyi <ms.hninwuttyi@gmail.com>
2024-06-13 23:31:38 +0700

    Complete Sql Injection

---
+++ HWYDotNetCore.WinFormsAppSqlInjection/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWYDotNetCore.WinFormsAppSqlInjection
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+        {
+            DataSource = "HNINWUTTYI\\MSSQLSERVER2012",
+            InitialCatalog = "HWYDotNetCore",
+            UserID = "sa",
+            Password = "admin123!",
+            TrustServerCertificate = true,
+        };
+
+    }
+}
---
+++ HWYDotNetCore.WinFormsAppSqlInjection/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace HWYDotNetCore.WinFormsAppSqlInjection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(212, 163);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(96, 86);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(323, 23);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(96, 115);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(323, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(548, 349);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(btnLogin);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+    }
+}
