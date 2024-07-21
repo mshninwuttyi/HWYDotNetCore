854e8b900e017e33b0cb1ec919663666653d1798
Wuttyi <ms.hninwuttyi@gmail.com>
2024-07-21 14:06:26 +0700

    [Complete] CanvasJS

---
+++ HWYDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HWYDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
