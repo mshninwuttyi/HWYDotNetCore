fe12bc1fc7ef53123a87447f77dc8458e37deddf
Wuttyi <ms.hninwuttyi@gmail.com>
2024-07-21 13:25:30 +0700

    [Complete] HighCharts

---
+++ HWYDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HWYDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
