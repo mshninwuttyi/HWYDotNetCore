5a5f3652f358b46a965c5b2eb8f610b468632fd8
Wuttyi <ms.hninwuttyi@gmail.com>
2024-07-23 00:19:16 +0700

    [Complete] Console App.ASP.NET Core Text Log

---
+++ HWYDotNetCore.ConsoleAppLogging/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/HWYDotNetCore.ConsoleAppLogging.txt", rollingInterval: RollingInterval.Hour)
+    .CreateLogger();
+
+Log.Fatal("Hello, World Fatal");
+Log.Error("Hello, World Error");
+Console.WriteLine("Hello, World!");
+int a = 10,
+    b = 0;
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
