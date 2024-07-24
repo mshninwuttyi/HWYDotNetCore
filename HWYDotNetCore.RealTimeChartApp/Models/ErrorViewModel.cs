72fcaa044b055e964fb57465dc0438fa59eab71e
Wuttyi <ms.hninwuttyi@gmail.com>
2024-07-24 23:58:00 +0700

    [Complete] Real Time Chat using Signal R

---
+++ HWYDotNetCore.RealTimeChartApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HWYDotNetCore.RealTimeChartApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
