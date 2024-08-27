ffcd05c52085a00f8393691a4915294559142193
Wuttyi <ms.hninwuttyi@gmail.com>
2024-08-27 13:22:11 +0700

    [Complete] ASP .Net Core MVC + UI Design Admin (LTE)

---
+++ WebAppHWYDotNetCore.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppHWYDotNetCore.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
