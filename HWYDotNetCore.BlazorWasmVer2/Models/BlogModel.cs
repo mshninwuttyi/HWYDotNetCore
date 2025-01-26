e1111de8d3e7301fb37c4b3c799b71458d7e59af
Wuttyi <ms.hninwuttyi@gmail.com>
2025-01-26 12:48:34 +0700

    feat: Create CRUD with Blazor Web Assembly

---
+++ HWYDotNetCore.BlazorWasmVer2/Models/BlogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWYDotNetCore.BlazorWasmVer2.Models;
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public string BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
