99c0f6a482abc342b9a7d52a4bd187cc333f55c7
Wuttyi <ms.hninwuttyi@gmail.com>
2025-02-11 00:21:50 +0700

    feat: Add Sign-in css file of bootstrap

---
+++ HWYDotNetCore.MiddlewareApp/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HWYDotNetCore.MiddlewareApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
