c1be23377002d15718ff9d94b47c630679141c3c
Wuttyi <ms.hninwuttyi@gmail.com>
2025-02-19 17:08:44 +0700

    feat: Add Login Model

---
+++ HWYDotNetCore.LoginAppV2/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HWYDotNetCore.LoginAppV2.Models
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string SessionId { get; set; }
+        public DateTime SessionExpire { get; set; }
+
+
+    }
+}
